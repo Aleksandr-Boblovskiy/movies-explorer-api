ede9033bdcf31b98d9c1d7fe4d458740b39862ff
Aleksandr Boblovskiy <developers3@gmail.com>
2021-10-07 09:55:24 +0300

    Update app.js

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const limiter = require('./middlewares/limiter');
 
 const app = express();
 app.use(cors());
+app.options('*', cors());
 
 mongoose.connect(MONGO_URL, {
   useNewUrlParser: true,
@@ -20,13 +21,13 @@ mongoose.connect(MONGO_URL, {
   useUnifiedTopology: true,
 });
 
-const allowedCors = [
-  'https://praktikum.tk',
-  'http://praktikum.tk',
-  'http://kinopoisk.nomoredomains.rocks',
-  'https://kinopoisk.nomoredomains.rocks',
-  'localhost:3000',
-];
+// const allowedCors = [
+//   'https://praktikum.tk',
+//   'http://praktikum.tk',
+//   'http://kinopoisk.nomoredomains.rocks',
+//   'https://kinopoisk.nomoredomains.rocks',
+//   'localhost:3000',
+// ];
 
 app.use(requestLogger);
 app.use(helmet());
@@ -34,15 +35,15 @@ app.use(limiter);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
-  // проверяем, что источник запроса есть среди разрешённых
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
+// app.use((req, res, next) => {
+//   const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+//   // проверяем, что источник запроса есть среди разрешённых
+//   if (allowedCors.includes(origin)) {
+//     res.header('Access-Control-Allow-Origin', origin);
+//   }
 
-  next();
-});
+//   next();
+// });
 
 app.use(routes);
 
