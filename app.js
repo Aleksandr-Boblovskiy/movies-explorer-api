2db5861973520155f0c1c23c98ab294414b2a4c3
Aleksandr Boblovskiy <developers3@gmail.com>
2021-10-07 09:34:03 +0300

    Update app.js

--- app.js
+++ app.js
@@ -20,11 +20,29 @@ mongoose.connect(MONGO_URL, {
   useUnifiedTopology: true,
 });
 
+const allowedCors = [
+  'https://praktikum.tk',
+  'http://praktikum.tk',
+  'http://kinopoisk.nomoredomains.rocks',
+  'https://kinopoisk.nomoredomains.rocks',
+  'localhost:3000',
+];
+
 app.use(requestLogger);
 app.use(helmet());
 app.use(limiter);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  // проверяем, что источник запроса есть среди разрешённых
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  next();
+});
 
 app.use(routes);
 
